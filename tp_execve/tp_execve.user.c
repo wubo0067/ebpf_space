87829ba67d5a17a625602fd7f908411e00b49481
calmwu <wubo0067@hotmail.com>
2021-08-27 16:14:27 +0800

    fix compile error

--- tp_execve/tp_execve.user.c
+++ tp_execve/tp_execve.user.c
@@ -2,7 +2,7 @@
  * @Author: CALM.WU
  * @Date: 2021-08-27 11:57:38
  * @Last Modified by: CALM.WU
- * @Last Modified time: 2021-08-27 14:13:56
+ * @Last Modified time: 2021-08-27 15:26:39
  */
 
 #include <stdio.h>
@@ -11,4 +11,7 @@
 #include "tp_execve.skel.h"
 #include "event.h"
 
-int32_t main( int32_t argc, char** argv ) { return 0; }
+int32_t main( int32_t argc, char** argv ) { 
+    fprintf( stderr, "main: %s\n", argv[0] );
+    return 0; 
+}
